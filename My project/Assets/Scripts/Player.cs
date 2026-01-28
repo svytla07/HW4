bbd61fc867360404001fd4a2b2b73c0de721ad42
svytla07 <vytlas@uci.edu>
2026-01-27 19:06:53 -0800

    Week 4 Commit

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2d;
+    
+    void Update()
+    {
+        
+    }
+}
